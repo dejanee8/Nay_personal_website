48a6bc2b5f13a80c31ccdd23f761048241996821
Dejanee Gomez <dejaneegomez@gmail.com>
2023-12-28 19:33:23 -0500

    add image and heading

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Crave the Relaxation";
 document.querySelector("body").appendChild(h2);
