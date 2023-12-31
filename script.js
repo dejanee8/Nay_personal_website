84d22c3c6c897c78d6d7c810b64637469725c65d
Dejanee Gomez <dejaneegomez@gmail.com>
2023-12-31 11:50:35 -0500

    Finish about section and contact section

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const h2 = document.createElement("h2");
-h2.textContent = "";
+h2.textContent = "This content added by Javascript";
 document.querySelector("body").appendChild(h2);
+
+const input = document.getElementById("button");
+input.addEventListener("click", function() {
+ alert("i was clicked!");
+});
+
+
+
+
